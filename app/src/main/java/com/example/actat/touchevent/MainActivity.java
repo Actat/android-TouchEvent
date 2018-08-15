d6b3d0af1c2a1247296c00a0bfd4d621cd4f9083
Actat <akiotoshima@gmail.com>
2018-08-15 16:37:10 +0900

    画面全体の中での座標をとれた

--- app/src/main/java/com/example/actat/touchevent/MainActivity.java
+++ app/src/main/java/com/example/actat/touchevent/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.actat.touchevent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        String action = "";
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                action = "ACTION_DOWN";
+                break;
+            case MotionEvent.ACTION_UP:
+                action = "ACTION_UP";
+                break;
+        }
+
+        Log.v("MotionEvent", "action = " + action + ", " + "x = " + String.valueOf(event.getX()) + ". " + "y = " + String.valueOf(event.getY()) + "\n");
+
+        return super.onTouchEvent(event);
     }
 }
