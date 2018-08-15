21b53464b1596ed92a9da04ed1fb6633d48e8e11
Actat <akiotoshima@gmail.com>
2018-08-15 20:06:19 +0900

    コンテンツの始まる高さを得ることができた

--- app/src/main/java/com/example/actat/touchevent/MainActivity.java
+++ app/src/main/java/com/example/actat/touchevent/MainActivity.java
@@ -1,21 +1,29 @@
 package com.example.actat.touchevent;
 
+import android.graphics.Rect;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private float dp = 0;
     private int layoutWidth = 0;
     private int layoutHeight = 0;
+    private int statusBarHeight = 0;
+    private int contentViewTop = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dp = getResources().getDisplayMetrics().density;
     }
 
     @Override
@@ -36,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
 
-        Log.v("MotionEvent", "action = " + action + "\t" +  "x = " + String.valueOf(event.getX()) + "  \t" + "y = " + String.valueOf(event.getY()) + "  \t" + "width = " + String.valueOf(layoutWidth) + ", height = " + String.valueOf(layoutHeight) + "\n");
+        Log.v("MotionEvent", "action = " + action + "\t" +  "x = " + String.valueOf(event.getX()) + "  \t" + "y = " + String.valueOf(event.getY()) + "  \t" + "width = " + String.valueOf(layoutWidth) + ", height = " + String.valueOf(layoutHeight) + "  \t" + "statusbarHeight = " + String.valueOf(statusBarHeight) + "  \t" + "contentTop = " + String.valueOf(contentViewTop) + "\n");
 
         return super.onTouchEvent(event);
     }
@@ -48,5 +56,10 @@ public class MainActivity extends AppCompatActivity {
         ConstraintLayout layout = findViewById(R.id.activity_main);
         layoutWidth = layout.getWidth();
         layoutHeight = layout.getHeight();
+
+        final Rect rect = new Rect();
+        getWindow().getDecorView().getWindowVisibleDisplayFrame(rect);
+        statusBarHeight = rect.top;
+        contentViewTop = (int)(getWindow().findViewById(Window.ID_ANDROID_CONTENT).getTop() * dp);
     }
 }
